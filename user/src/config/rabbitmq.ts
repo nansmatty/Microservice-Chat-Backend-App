10af85708d61d1a5f5ee91f643fe25c23a1984c5
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-09-12 20:08:19 +0530

    feat: implement RabbitMQ connection and message publishing functionality

--- user/src/config/rabbitmq.ts
+++ user/src/config/rabbitmq.ts
@@ -14,7 +14,17 @@ export const connectRabbitMQ = async () => {
 		channel = await connection.createChannel();
 		console.log('✅Connected to RabbitMQ.');
 	} catch (error) {
-		console.error('Error connecting to RabbitMQ:', error);
+		console.error('‼️Error connecting to RabbitMQ:', error);
 		process.exit(1);
 	}
+};
+
+export const publishToQueue = async (queueName: string, message: any) => {
+	if (!channel) {
+		throw new Error('‼️RabbitMQ channel is not established');
+	}
+	await channel.assertQueue(queueName, { durable: true });
+	channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+		persistent: true,
+	});
 };
