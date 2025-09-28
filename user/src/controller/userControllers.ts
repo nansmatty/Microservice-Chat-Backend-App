6f5c1ace036431e7c76f89524f81992e65c854e6
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-09-28 21:19:35 +0530

    refactor(user): simplify response structure in getUser endpoint

--- user/src/controller/userControllers.ts
+++ user/src/controller/userControllers.ts
@@ -95,5 +95,5 @@ export const getUser = TryCatch(async (req: AuthRequest, res) => {
 	if (!user) {
 		return res.status(404).json({ message: 'User not found' });
 	}
-	res.status(200).json({ user });
+	res.status(200).json(user);
 });
