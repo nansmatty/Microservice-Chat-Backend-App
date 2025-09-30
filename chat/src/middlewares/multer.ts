3d31a682a0434cc1ff45eb84f98fc6942e9650c6
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-09-30 19:10:22 +0530

    fix on multer

--- chat/src/middlewares/multer.ts
+++ chat/src/middlewares/multer.ts
@@ -15,7 +15,7 @@ const upload = multer({
 	storage: storage,
 	limits: { fileSize: 5 * 1024 * 1024 },
 	fileFilter: (req, file, cb) => {
-		if (!file.mimetype.startsWith('/image/')) {
+		if (!file.mimetype.startsWith('image/')) {
 			return cb(new Error('Only image files are allowed!'));
 		} else {
 			cb(null, true);
