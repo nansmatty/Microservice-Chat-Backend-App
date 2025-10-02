805ab01f4af9e46d9a87b213f3a562ea07780a46
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-10-02 19:21:05 +0530

    user handshaking, join and leave chat as well as online status and typing effect featured implemented

--- chat/src/config/socket.ts
+++ chat/src/config/socket.ts
@@ -17,8 +17,47 @@ const userSocketMap: Record<string, string> = {};
 io.on('connection', (socket: Socket) => {
 	console.log('User connected:', socket.id);
 
+	const userId = socket.handshake.query.userId as string | undefined;
+
+	if (userId && userId !== 'undefined') {
+		userSocketMap[userId] = socket.id;
+		console.log(`User ID ${userId} mapped to socket ID ${socket.id}`);
+	}
+
+	io.emit('getOnlineUser', Object.keys(userSocketMap));
+
+	if (userId) {
+		socket.join(userId);
+	}
+
+	socket.on('typing', (data) => {
+		console.log(`User ${data.userId} is typing in chat ${data.chatId}`);
+		socket.to(data.chatId).emit('userTyping', { userId: data.userId, chatId: data.chatId });
+	});
+
+	socket.on('stopTyping', (data) => {
+		console.log(`User ${data.userId} stopped typing in chat ${data.chatId}`);
+		socket.to(data.chatId).emit('userStoppedTyping', { userId: data.userId, chatId: data.chatId });
+	});
+
+	socket.on('joinChat', (chatId) => {
+		socket.join(chatId);
+		console.log(`User ${userId} joined chat room ${chatId}`);
+	});
+
+	socket.on('leaveChat', (chatId) => {
+		socket.leave(chatId);
+		console.log(`User ${userId} left chat room ${chatId}`);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected:', socket.id);
+
+		if (userId) {
+			delete userSocketMap[userId];
+			console.log(`User ID ${userId} removed from mapping`);
+			io.emit('getOnlineUser', Object.keys(userSocketMap));
+		}
 	});
 
 	socket.on('connect_error', (error) => {
